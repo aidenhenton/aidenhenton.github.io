58312c0efc70f01393102437a122dbab5e13f831
aidenhenton <aidenhenton21@gmail.com>
2026-09-08 19:13:04 +0000

    Finalized platformer game

--- fsd-projects/platformer/platformer.js
+++ fsd-projects/platformer/platformer.js
@@ -18,7 +18,13 @@ $(function () {
 
     // Create walls - do not delete or modify this code
     createPlatform(-50, -50, canvas.width + 100, 50); // top wall
-    createPlatform(-50, canvas.height - 10, canvas.width + 100, 200, "rgb(118, 0, 233)"); // bottom wall
+    createPlatform(
+      -50,
+      canvas.height - 10,
+      canvas.width + 100,
+      200,
+      "rgb(118, 0, 233)",
+    ); // bottom wall
     createPlatform(-50, -50, 50, canvas.height + 500); // left wall
     createPlatform(canvas.width, -50, 50, canvas.height + 100); // right wall
 
@@ -27,24 +33,29 @@ $(function () {
     //////////////////////////////////
 
     // TODO 1 - Enable the Grid
-    // toggleGrid();
-
+    toggleGrid();
 
     // TODO 2 - Create Platforms
-
-
-
+    
+    createPlatform(0, 650, 300, 100, "#35a7ff");
+    createPlatform(400, 560, 200, 300, "#ffd166");
+    createPlatform(600, 470, 180, 600, "#ffd166");
+    createPlatform(900, 380, 700, 30, "#06d6a0");
+    createPlatform(840, 520, 700, 3000, "#06d6a0");
+    createPlatform(1200, 380, 700, 3000, "#06d6a0");
+    createPlatform(1080, 200, 180, 30, "#ef476f");
+    createPlatform(460, 250, 140, 30, "#9b5de5");
 
     // TODO 3 - Create Collectables
-
-
-
-    
+  createCollectable("food", 1150, 450);
+  createCollectable("steve", 1350, 50);
+  createCollectable("diamond", 1150, 1, 0.5, 0.7);
+  createCollectable("database", 500, 400, 0.5, 0.7);
+  createCollectable("drink", 510, 148, 0.5, 0.7);
     // TODO 4 - Create Cannons
-
-
-    
-    
+  createCannon("left", 400, 15000, 4000, 20000)
+  createCannon("right", 900, 100, 4000, 200)
+  createCannon("right", 800, 5000, 600, 150)
     //////////////////////////////////
     // ONLY CHANGE ABOVE THIS POINT //
     //////////////////////////////////
--- fsd-projects/platformer/setup.js
+++ fsd-projects/platformer/setup.js
@@ -3,7 +3,7 @@ const walkAcceleration = 2.5; // how much is added to the speed each frame
 const gravity = 0.5; // how much is subtracted from speedY each frame
 const friction = 1.5; // how much the player is slowed each frame
 const maxSpeed = 8; // maximum horizontal speed, not vertical
-const playerJumpStrength = 12; // this is subtracted from the speedY each jump
+const playerJumpStrength = 14; // this is subtracted from the speedY each jump
 const projectileSpeed = 8; // the speed of projectiles
 let shouldDrawGrid = false;
 let gridMade = false;
@@ -102,4 +102,6 @@ var collectableList = {
   kennedi: { image: "images/collectables/kennedi-head.png" },
   max: { image: "images/collectables/max-head.png" },
   steve: { image: "images/collectables/steve-head.png" },
+  food: { image: "images/collectables/baby-food.png" },
+  drink: { image: "images/collectables/feeding-bottle.png" },
 };
